5168f8f037ee871da2c3533d5ac82eb0e3346a21
kranthi <g.kranthi2507@gmail.com>
2020-06-29 06:43:18 +0000

    add oauth

---
+++ userapp/interactors/presenters/presenter_interface.py
@@ -0,0 +1,22 @@
+from abc import ABC, abstractmethod
+
+from typing import List
+
+
+class PresenterInterface(ABC):
+
+    @abstractmethod
+    def raise_invalid_username(self):
+        pass
+
+    @abstractmethod
+    def raise_invalid_username_and_password(self):
+        pass
+
+    @abstractmethod
+    def raise_exception_for_invalid_user(self):
+        pass
+
+    @abstractmethod
+    def get_user_response(self, user_dtos: List[UserDto]):
+        pass
