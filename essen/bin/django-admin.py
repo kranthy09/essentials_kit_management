413c021d9bf98aa1cffb7c13fc6ac3d5f2d13ca0
skywalker <g.kranthi2507@gmail.com>
2020-07-04 15:42:35 +0530

    add essen virtual env

---
+++ essen/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/kranthi/Desktop/iB Training/projects/essentials_kit_management/essen/bin/python3.8
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
